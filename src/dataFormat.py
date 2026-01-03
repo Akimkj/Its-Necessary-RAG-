7195cb0954ed8a8e4c0e391cc47d0811058f3715
Mika <kakamarques.0202@gmail.com>
2026-01-03 03:29:34 -0300

    refactor: mudança do padrão pydantic: de expectedQuestion para question, e de expectedAnswer para answer

--- src/dataFormat.py
+++ src/dataFormat.py
@@ -1,23 +1,23 @@
 from pydantic import BaseModel, Field, field_validator
-from src.utils import clean_text 
+import utils 
 from typing import List
 
 
 class QAPairModel(BaseModel):
     id: int = Field(gt=0)
-    expectedQuestion: str = Field(strip_whitespace=True)
-    expectedAnswer: str = Field(strip_whitespace=True)
+    question: str = Field(strip_whitespace=True)
+    answer: str = Field(strip_whitespace=True)
     
 
 
-    @field_validator('expectedQuestion', 'expectedAnswer', mode='before')
+    @field_validator('question', 'answer', mode='before')
     @classmethod
     def validade_content(cls, text: str):
 
         if not isinstance(text, str):
             text = str(text)
 
-        new_text = clean_text(text)
+        new_text = utils.clean_text(text)
 
         if not new_text:
             raise ValueError("O campo não pode ser vazio")
@@ -26,10 +26,10 @@ class QAPairModel(BaseModel):
         
         return new_text
     
-    @field_validator('expectedAnswer', mode='after')
+    @field_validator('answer', mode='after')
     @classmethod
     def check_final_answer(cls, answer: str):
-        if len(answer.split()) < 10:
+        if len(answer.split()) < 2:
             raise ValueError(f"Resposta esperada \n'{answer}'\n tem menos de 10 palavras, muito pequena")
         return answer
 

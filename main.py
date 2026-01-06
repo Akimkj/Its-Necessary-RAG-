2d886d23d233c61a95aa891e6df818789a69e107
Mika Marques <kakamarques.0202@gmail.com>
2026-01-06 13:18:02 -0300

    feat: vai adicionar as funções de comparação dos Dataframes gerados pelo arquivo evaluation.py, formando, por fim, um gráfico em barras comparando o bertScore dos dois prompts do gemini sem RAG

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from src.geminiSetGenerator import process_questions
-from src.utils import loadData
+#from src.geminiSetGenerator import process_questions
+from src.utils import loadData, bertCompareGraph
 import os
+from src.evaluation import bertEvaluation
+import pandas as pd
 
-PATH_GOLDENSET = os.path.join("data", "stackoverflow_dataset.json")
+#carrega os caminhos dos datasets
+PATH_GOLDENSET = os.path.join("data", "stackoverflow_dataset.json") 
+PATH_GEMINISET1 = os.path.join("data", "gemini_dataset_v1.json")
+PATH_GEMINISET2 = os.path.join("data", "gemini_dataset_v2.json")
+PATH_GEMINIBERT1 = os.path.join("results", "avBert_gemini_v1.csv")
+PATH_GEMINIBERT2 = os.path.join("results", "avBert_gemini_v2.csv")
 
-rawgolden = loadData(PATH_GOLDENSET, default_type=list)
+'''
+#Carrega os dados dos datasets da forma que estão disponíveis
+rawGolden = loadData(PATH_GOLDENSET, default_type=list)
+rawGemini1 = loadData(PATH_GEMINISET1)
+rawGemini2 = loadData(PATH_GEMINISET2)
 
-if rawgolden:
-    process_questions(rawgolden)   
+bertEvaluation(rawGemini1, rawGolden)
+bertEvaluation(rawGemini2, rawGolden)
+'''
+
+df1 = pd.read_csv(PATH_GEMINIBERT1)
+df2 = pd.read_csv(PATH_GEMINIBERT2)
+
+bertCompareGraph(df1, df2)
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
-import json, re
+import matplotlib.pyplot as plt
+import json, re, os
+import pandas as pd
 
 def clean_text(text: str):
     text_cleaned = re.sub(r'\s+', ' ', text).strip()
@@ -17,3 +19,19 @@ def loadData(filePath: str, default_type=dict):
     except json.JSONDecodeError:
         print(f"Erro ao decodificar JSON do arquivo {filePath}")
         return default_type()
+
+
+def bertCompareGraph(df1, df2):
+    pathResult = os.path.join("results", "avBert_prompt_compare.png")
+
+    cleanV1 = df1[['id', 'bert_F1']].rename(columns={'bert_F1': 'F1_v1'})
+    cleanV2 = df2[['id', 'bert_F1']].rename(columns={'bert_F1': 'F1_v2'})
+
+
+    df = pd.merge(cleanV1, cleanV2, on='id')
+    df.set_index('id').plot.bar(figsize=(14, 7))
+    plt.ylim(0.000, 1.0)
+    plt.tight_layout()
+
+    plt.savefig(pathResult, dpi=300, bbox_inches='tight')
+    plt.show()
